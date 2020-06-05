c57bba6b62238f77355c9c77565259ba47bc0a98
Ben Highsted <benhighsted@Bens-MacBook-Pro.local>
2020-06-05 22:07:05 +1200

    Created a new board class, set up positions

---
+++ Chess/Board.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Chess
+{
+    public class Board
+    {
+        public Board(List<GameTile> board, int boardSize)
+        {
+            setupBoard(board, boardSize);
+        }
+
+        private static void initialiseBoard(List<GameTile> board, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+
+                    GameTile tile = new GameTile();
+
+                    tile.col = i;
+                    tile.row = j;
+                    tile.type = "P";
+
+                    board.Add(tile);
+                }
+            }
+        }
+
+        public static void setupBoard(List<GameTile> board, int boardSize)//this method will setup the board for a new game
+        {
+            initialiseBoard(board, boardSize);
+        }
+    }
+}
--- Chess/Program.cs
+++ Chess/Program.cs
@@ -5,50 +5,33 @@ namespace Chess
 {
     class MainClass
     {
-        private static List<GameTile> board = new List<GameTile>();
+        public static List<GameTile> board = new List<GameTile>();
 
         public static void Main(string[] args)
         {
             int boardSize = 8;
-            initialiseBoard(boardSize);
-            printBoard();
 
+            new Board(board, boardSize);
+            printBoard(board);
         }
 
-        //will probably move these to a seperate file (where all the board related stuff will be located
-        private static void initialiseBoard(int size) {
-            for (int i = 0; i < size; i++) {
-                for (int j = 0; j < size; j++) {
-
-                    GameTile tile = new GameTile();
-
-                    tile.col = i;
-                    tile.row = j;
-                    tile.type = "P";//this will need to be the piece type. (i.e. pawn)
-
-                    board.Add(tile);
-                }
-            }
-        }
-
-        private static void printBoard()
+        private static void printBoard(List<GameTile> board)
         {
             foreach (GameTile tile in board)
             {
-                Console.Write(tile.type + " ");
+                //Console.Write(tile.type + " ");
+                Console.Write(tile.row.ToString() + " ");
                 if (tile.row == 7)
                     Console.WriteLine();
             }
         }
-
-        private static void setupBoard() { }//this method will setup the board for a new game
-
     }
 
-    public class GameTile
+    public class GameTile //thinking i'll need a seperate class for each piece
     {
         public int row;
-        public int col;
-        public String type;
+        public int col;//board positions
+
+        public string type;//piece type (i.e. pawn)
     }
 }
