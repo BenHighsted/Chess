7ea9842f7154fb67ee13739e1893f6f2f746320a
BenHighsted <benhigh18@gmail.com>
2020-06-08 12:38:53 +1200

    Added move class which will contain move functions, other misc changes

---
+++ Chess/Move.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class Move
+    {
+        public static bool MovePiece(int boardPos, int newBoardPos) {
+            //thinking that i will get the reference at boardPos, which will give the piece type
+            //then I will validate the move (i.e. if its null, or if it can/can not take that piece)
+            //so in other words, when this is called, its saying the piece at boardPos, wants to move to newBoardPos
+
+
+            return false;
+        }
+    }
+}
